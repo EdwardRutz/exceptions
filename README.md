5cb888df23e8d0f8d9876a67b7a35a1d714d9ce2
Edward R <edwardrz@users.noreply.github.com>
2017-10-15 20:39:01 -0500

    Updated and completed.

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # Exception Handling
+Back-End Exercise, Nashville Software School
+
+## Objective
+- Use best practices to handle all exceptions from main.rb. Do not change the code. 
+
+## Concepts
+Error/Exceptions Handling
 
 
-## Instructions
+## Requirements
 
-- [ ] Run main.rb and handle all raised exceptions. Pay attention to the comments that provide additional info/directions. You should not change the code to fix the errors but handle all exceptions with proper exception handling.
+- [X] Run main.rb and handle all raised exceptions. Pay attention to the comments that provide additional info/directions. 
+  - You should not change the code to fix the errors but handle all exceptions with proper exception handling.
 
 
 ## References
-
-
+- [Ruby-Docs 2.2.4: Exception](http://ruby-doc.org/core-2.4.2/Exception.html)
+- [Ruby Learning: Exceptions](http://rubylearning.com/satishtalim/ruby_exceptions.html)
+- [SitePoint: Error Handling, Beyond the Basics](https://www.sitepoint.com/ruby-error-handling-beyond-basics/)
+- [The Magic Tricks of Testing](https://www.youtube.com/watch?v=URSWYvyc42M)
+- [Introduction to Mini Test](https://launchschool.com/blog/assert-yourself-an-introduction-to-minitest)
+- [TH Exception Handling](https://teamtreehouse.com/library/basic-exception-handling-with-php)
--- main.rb
+++ main.rb
@@ -1,11 +1,47 @@
+# Exception Handling:  
+# Use best practices to handle all exceptions from main.rb. Do not change the code. 
+
 require_relative 'patient'
 
-    new_patient = Patient.new("Jisie", "David")
-    puts new_patient.first_name
-    puts new_patient.last_name
+new_patient = Patient.new("Darth", "Vader")
+puts new_patient.first_name # No errors
+puts new_patient.last_name  # No errors
+
+begin 
     puts patient_contact_info
+rescue NameError
+    puts "ERROR 100: undefined local variable or method `patient_contact_info'(NameError)"
+end
+
+begin 
     puts new_patient.patient_status(:coma)
+rescue ArgumentError
+    puts "ERROR 200: Argument is not a string (ArgumentError)"
+end 
+
+begin
     puts new_patient.patient_currently_admitted?
+rescue ArgumentError
+    puts "ERROR 201: in `patient_currently_admitted?': wrong number of arguments (given 0, expected 1) (ArgumentError)"
+end
+
+begin
     puts new_patient.patient_new_total(125)
+rescue ZeroDivisionError
+    puts "ERROR 400: divided by 0 (ZeroDivisionError)"
+end
+
+begin
     new_patient.add_med_to_list("lipitor", 123.12)
+rescue ArgumentError
+    puts "ERROR 202: in `add_med_to_list': 123.12 is not a number (ArgumentError)"
+end
+
+begin
     new_patient.patient_medications_list
+rescue RuntimeError
+    puts "ERROR 600: in `patient_medications_list': Medications list is not valid. (RuntimeError)"
+end
+
+
+
